2e6b386efa901c9601f1188fdaca0763be485e72
Yifei Xu <43258911+Xu-Yifei@users.noreply.github.com>
2018-10-21 09:45:27 -0400

    Update gmapsbody.js

--- gmapsbody.js
+++ gmapsbody.js
@@ -1,40 +1,64 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
 var map, infoWindow;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.397, lng: 257.644},
-    zoom: 3.7
-  });
-  infoWindow = new google.maps.InfoWindow;
+	var latitude, longitude;
+	var myLatLng = {lat: latitude, lng: longitude};
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+ 	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 51.397, lng: 257.644},
+		zoom: 3.7
+  	});
+  	infoWindow = new google.maps.InfoWindow;
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found');
-      infoWindow.open(map);
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
+  	// Try HTML5 geolocation.
+  	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+	  	var pos = {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude
+		};
+
+		infoWindow.setPosition(pos);
+		infoWindow.setContent('Location found');
+	  	infoWindow.open(map);
+	  	map.setCenter(pos);
+	}, function() {
+		handleLocationError(true, infoWindow, map.getCenter());
+	});
+  	} else {
+	// Browser doesn't support Geolocation
+	handleLocationError(false, infoWindow, map.getCenter());
+  	}
+
+  	// Update lat/long value of div when anywhere in the map is clicked    
+    google.maps.event.addListener(map,'click',function(event) {                
+        document.getElementById('coordinates').innerHTML = "Coordinates: " + event.latLng.lat() + ", " + event.latLng.lng();
+
+	    
+	    getData(event.latLng.lat(),event.latLng.lng()).then(function(citymap) {
+	      	// Construct the circle for each value in map.
+		    for (var city in citymap) {
+		      	// Add the circle for this city to the map.
+		      	var cityCircle = new google.maps.Circle({
+		        	strokeColor: 555555,
+		        	strokeOpacity: 0.8,
+		        	strokeWeight: 0.35,
+		        	fillColor: colour(citymap[city]),
+		        	fillOpacity: 0.35,
+		        	map: map,
+		        	center: citymap[city].center,
+		        	radius: 50000
+		      	});
+		    }
+		});
     });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
+  	infoWindow.setPosition(pos);
+  	infoWindow.setContent(browserHasGeolocation ?
+		'Error: The Geolocation service failed.' :
+		'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
 }
