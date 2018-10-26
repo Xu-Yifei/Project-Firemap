6bbda796f2cbe20365438908b1bd6626366d63fe
kylemtam <43300149+kylemtam@users.noreply.github.com>
2018-10-26 10:36:54 -0400

    Update gmapsbodytwo.js

--- gmapsbodytwo.js
+++ gmapsbodytwo.js
@@ -1,68 +1,72 @@
 var map;
-   var marker;
-   var infowindow;
-   var messagewindow;
+var marker;
+var infowindow;
+var messagewindow;
 
-   function initMap() {
-     var home = {lat: -34.397, lng: 150.644};
-     map = new google.maps.Map(document.getElementById('map'), {
-       center: home,
-       zoom: 4.5
-     });
+function initMap() {
+  var home = { lat: -34.397, lng: 150.644 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: home,
+    zoom: 4.5
+  });
 
-     infowindow = new google.maps.InfoWindow({
-       content: document.getElementById('form')
-     });
+  infowindow = new google.maps.InfoWindow({
+    content: document.getElementById("form")
+  });
 
-     messagewindow = new google.maps.InfoWindow({
-       content: document.getElementById('message')
-     });
+  messagewindow = new google.maps.InfoWindow({
+    content: document.getElementById("message")
+  });
 
-     google.maps.event.addListener(map, 'click', function(event) {
-       marker = new google.maps.Marker({
-         position: event.latLng,
-         map: map
-       });
+  google.maps.event.addListener(map, "click", function(event) {
+    marker = new google.maps.Marker({
+      position: event.latLng,
+      map: map
+    });
 
+    google.maps.event.addListener(marker, "click", function() {
+      infowindow.open(map, marker);
+      form.hidden = false;
+    });
+  });
+}
 
-       google.maps.event.addListener(marker, 'click', function() {
-         infowindow.open(map, marker);
-         form.hidden = false;
-       });
-     });
-   }
+function saveData() {
+  var location = escape(document.getElementById("Location").value);
+  var type = document.getElementById("type").value;
+  var latlng = marker.getPosition();
+  var url =
+    "phpsqlinfo_addrow.php?Location=" +
+    location +
+    "&lat=" +
+    latlng.lat() +
+    "&lng=" +
+    latlng.lng() +
+    "&type=" +
+    type;
 
-   function saveData() {
-     var location = escape(document.getElementById('Location').value);
-     var type = document.getElementById('type').value;
-     var latlng = marker.getPosition();
-     var url = 'phpsqlinfo_addrow.php?Location=' + location +
-               '&lat=' + latlng.lat() + '&lng=' + latlng.lng() + '&type=' + type;
+  downloadUrl(url, function(data, responseCode) {
+    if (responseCode == 200 && data.length <= 1) {
+      infowindow.close();
+      messagewindow.open(map, marker);
+    }
+  });
+}
 
-     downloadUrl(url, function(data, responseCode) {
+function downloadUrl(url, callback) {
+  var request = window.ActiveXObject
+    ? new ActiveXObject("Microsoft.XMLHTTP")
+    : new XMLHttpRequest();
 
-       if (responseCode == 200 && data.length <= 1) {
-         infowindow.close();
-         messagewindow.open(map, marker);
-       }
-     });
-   }
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      request.onreadystatechange = doNothing;
+      callback(request.responseText, request.status);
+    }
+  };
 
-   function downloadUrl(url, callback) {
-     var request = window.ActiveXObject ?
-         new ActiveXObject('Microsoft.XMLHTTP') :
-         new XMLHttpRequest;
+  request.open("GET", url, true);
+  request.send(null);
+}
 
-     request.onreadystatechange = function() {
-       if (request.readyState == 4) {
-         request.onreadystatechange = doNothing;
-         callback(request.responseText, request.status);
-       }
-     };
-
-     request.open('GET', url, true);
-     request.send(null);
-   }
-
-   function doNothing () {
-   }
+function doNothing() {}
